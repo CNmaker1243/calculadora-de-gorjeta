4a290ec76ad951df44b859d740044092ddf12c29
Gustavo Tavares <gutoarcangelo@gmail.com>
2024-03-31 23:58:02 -0300

    nono commit do CSS e terceiro commit do JS

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let conta = 0
 let pessoas = 0
+let porcentagem = 0
 
 const containput = document.querySelector("#conta")
 containput.addEventListener("input", receberValorConta)
@@ -14,6 +15,7 @@ pessoasInput.addEventListener("input", )
 function receberQuantidadePessoas(evento) {
     const paragrafoErro = document.querySelector (".pessoas #erro")
     const divErro = document.querySelector(".pessoas .input-box")
+    
     if(evento.target.value === 0) {
         paragrafoErro.style.display = "block"
         divErro.setAttribute("id", "erro-div")
@@ -24,4 +26,19 @@ function receberQuantidadePessoas(evento) {
     }
 }
 
-const botoesGorjeta = document.querySelector(".gorjeta input[type= 'number']")
+const botoesGorjeta = document.querySelectorAll(".gorjeta input[type='button']")
+botoesGorjeta.forEach(botao => {
+    botao.addEventListener("click", receberPorcentagemBotao)
+})
+
+function receberPorcentagemBotao(evento) {
+    botoesGorjeta.forEach(botao => {
+        botao.classList.remove("botao-ativo")
+        
+        if(botao.value === evento.target.value){
+            botao.classList.add("botao-ativo")
+        }
+    })
+
+    porcentagem = parseFloat(evento.target.value) / 100
+}
